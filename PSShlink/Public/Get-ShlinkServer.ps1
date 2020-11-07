7a13fc82ac142c0ab4e79805b751ee742b7136b7
Adam Cook <me@cookadam.co.uk>
2020-11-07 10:40:37 +0000

    Added CBH for Get-ShlinkServer

--- PSShlink/Public/Get-ShlinkServer.ps1
+++ PSShlink/Public/Get-ShlinkServer.ps1
@@ -1,4 +1,20 @@
 function Get-ShlinkServer {
+    <#
+    .SYNOPSIS
+        Checks the healthiness of the service, making sure it can access required resources.
+    .DESCRIPTION
+        Checks the healthiness of the service, making sure it can access required resources.
+
+        https://api-spec.shlink.io/#/Monitoring/health
+    .EXAMPLE
+        PS C:\> Get-ShlinkServer
+        
+        Returns the healthiness of the service.
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding()]
     param (
         [Parameter()]
