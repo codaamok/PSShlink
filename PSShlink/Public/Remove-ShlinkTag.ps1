dc3feb938d1fd661b31ae11fcd336e365ce87955
Adam Cook <me@cookadam.co.uk>
2020-11-08 22:59:27 +0000

    Added CBH for Remove-ShlinkTag

--- PSShlink/Public/Remove-ShlinkTag.ps1
+++ PSShlink/Public/Remove-ShlinkTag.ps1
@@ -1,4 +1,30 @@
 function Remove-ShlinkTag {
+    <#
+    .SYNOPSIS
+        Remove a tag from an existing Shlink server.
+    .DESCRIPTION
+        Remove a tag from an existing Shlink server.
+    .PARAMETER Tags
+        Name(s) of the tag(s) you want to remove.
+    .PARAMETER ShlinkServer
+        The URL of your Shlink server (including schema). For example "https://example.com".
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .PARAMETER ShlinkApiKey
+        A SecureString object of your Shlink server's API key.
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .EXAMPLE
+        PS C:\> Remove-ShlinkTag -Tags "oldwebsite" -WhatIf
+        
+        Reports what would happen if the command was invoked, because the -WhatIf parameter is present.
+    .EXAMPLE
+        PS C:\> Remove-ShlinkTag -Tags "oldwebsite", "newwebsite"
+
+        Removes the following tags from the Shlink server: "oldwebsite", "newwebsite"
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
     param (
         [Parameter(Mandatory)]
