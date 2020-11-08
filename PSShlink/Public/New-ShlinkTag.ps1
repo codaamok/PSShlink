47be25f09aef9f4e70ec0d627c772ce702b7279d
Adam Cook <me@cookadam.co.uk>
2020-11-08 22:39:46 +0000

    Added CBF for New-ShlinkTag

--- PSShlink/Public/New-ShlinkTag.ps1
+++ PSShlink/Public/New-ShlinkTag.ps1
@@ -1,4 +1,20 @@
 function New-ShlinkTag {
+    <#
+    .SYNOPSIS
+        Creates one or more new tags on your Shlink server
+    .DESCRIPTION
+        Creates one or more new tags on your Shlink server
+    .PARAMETER Tags
+        Name(s) for your new tag(s)
+    .EXAMPLE
+        PS C:\> New-ShlinkTag -Tags "oldwebsite","newwebsite","misc"
+        
+        Creates the following new tags on your Shlink server: "oldwebsite","newwebsite","misc"
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
