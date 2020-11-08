38b755fe21c5c5fde0f07dfb5c2bb6e5d768bc22
Adam Cook <me@cookadam.co.uk>
2020-11-08 22:42:00 +0000

    Updated CBF for New-ShlinkTag

--- PSShlink/Public/New-ShlinkTag.ps1
+++ PSShlink/Public/New-ShlinkTag.ps1
@@ -6,6 +6,12 @@ function New-ShlinkTag {
         Creates one or more new tags on your Shlink server
     .PARAMETER Tags
         Name(s) for your new tag(s)
+    .PARAMETER ShlinkServer
+        The URL of your Shlink server (including schema). For example "https://example.com".
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .PARAMETER ShlinkApiKey
+        A SecureString object of your Shlink server's API key.
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
     .EXAMPLE
         PS C:\> New-ShlinkTag -Tags "oldwebsite","newwebsite","misc"
         
