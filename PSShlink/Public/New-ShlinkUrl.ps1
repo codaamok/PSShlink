bec50a8c4d78ea00620fd44136d591e051e18582
Adam Cook <me@cookadam.co.uk>
2020-11-08 22:56:19 +0000

    Added CBH for New-ShlinkUrl

--- PSShlink/Public/New-ShlinkUrl.ps1
+++ PSShlink/Public/New-ShlinkUrl.ps1
@@ -1,4 +1,47 @@
 function New-ShlinkUrl {
+    <#
+    .SYNOPSIS
+        Creates a new Shlink short code on your Shlink server.
+    .DESCRIPTION
+        Creates a new Shlink short code on your Shlink server.
+    .PARAMETER LongUrl
+        Define the long URL for the new short code.
+    .PARAMETER CustomSlug
+        Define a custom slug for the new short code.
+    .PARAMETER Tags
+        Associate tag(s) with the new short code.
+    .PARAMETER ValidSince
+        Define a "valid since" date with the new short code.
+    .PARAMETER ValidUntil
+        Define a "valid until" date with the new short code.
+    .PARAMETER MaxVisits
+        Set the maximum number of visits allowed for the new short code.
+    .PARAMETER Domain
+        Associate a domain with the new short code to be something other than the default domain. 
+        This is useful if your Shlink instance is responding/creating short URLs for multiple domains.
+    .PARAMETER ShortCodeLength
+        Set the length of your new short code other than the default.
+    .PARAMETER FindIfExists
+        Specify this switch to first search and return the data about an existing short code that uses the same long URL if one exists.
+    .PARAMETER ShlinkServer
+        The URL of your Shlink server (including schema). For example "https://example.com".
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .PARAMETER ShlinkApiKey
+        A SecureString object of your Shlink server's API key.
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .EXAMPLE
+        PS C:\> New-ShlinkUrl -LongUrl "https://google.com"
+        
+        Will generate a new short code with the long URL of "https://google.com", using your Shlink server's default for creating new short codes, and return all the information about the new short code.
+    .EXAMPLE
+        PS C:\> New-ShlinkUrl -LongUrl "https://google.com" -CustomSlug "mygoogle" -Tags "search-engine" -ValidSince (Get-Date "2020-11-01") -ValidUntil (Get-Date "2020-11-30") -MaxVisits 99 -FindIfExists
+    
+        Will generate a new short code with the long URL of "https://google.com" using the custom slug "search-engine". The default domain for the Shlink server will be used. The link will only be valid for November 2020. The link will only work for 99 visits. If a duplicate short code is found using the same long URL, another is not made and instead data about the existing short code is returned.
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
