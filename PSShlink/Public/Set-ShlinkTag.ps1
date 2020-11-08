e1a8a4a6a01b292f9c30d8eb36c21831ff70a6ed
Adam Cook <me@cookadam.co.uk>
2020-11-08 23:04:47 +0000

    Added CBH for Set-ShlinkTag

--- PSShlink/Public/Set-ShlinkTag.ps1
+++ PSShlink/Public/Set-ShlinkTag.ps1
@@ -1,4 +1,27 @@
 function Set-ShlinkTag {
+    <#
+    .SYNOPSIS
+        Renames an existing tag to a new value on the Shlink server.
+    .DESCRIPTION
+        Renames an existing tag to a new value on the Shlink server.
+    .PARAMETER OldTagName
+        The name of the old tag you want to change the name of.
+    .PARAMETER NewTagName
+        The name fo the new tag you want to the new name to be.
+    .PARAMETER ShlinkServer
+        The URL of your Shlink server (including schema). For example "https://example.com".
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .PARAMETER ShlinkApiKey
+        A SecureString object of your Shlink server's API key.
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
