7539c8d8d0d1ebac08f39bd5e08436ca01665dce
Adam Cook <me@cookadam.co.uk>
2020-11-08 22:36:30 +0000

    Added CBH for Ge-ShlinkVisits

--- PSShlink/Public/Get-ShlinkVisits.ps1
+++ PSShlink/Public/Get-ShlinkVisits.ps1
@@ -1,4 +1,47 @@
 function Get-ShlinkVists {
+    <#
+    .SYNOPSIS
+        Short description
+    .DESCRIPTION
+        Long description
+    .PARAMETER ShortCode
+        The name of the short code you wish to return the visits data for. For example, if the short URL is "https://example.com/new-url" then the short code is "new-url".
+    .PARAMETER Tag
+        The name of the tag you wish to return the visits data for.
+    .PARAMETER Domain
+        The domain (excluding schema) associated with the short code you wish to search for. For example, "example.com" is an acceptable value. 
+        This is useful if your Shlink instance is responding/creating short URLs for multiple domains.
+    .PARAMETER StartDate
+        A datetime object to filter the visit data where the start date is equal or greater than this value. 
+    .PARAMETER EndDate
+        A datetime object to filter the visit data where its end date is equal or less than this value. 
+    .PARAMETER ShlinkServer
+        The URL of your Shlink server (including schema). For example "https://example.com".
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .PARAMETER ShlinkApiKey
+        A SecureString object of your Shlink server's API key.
+        It is not required to use this parameter for every use of this function. When it is used once for any of the functions in the PSShlink module, its value is retained throughout the life of the PowerShell session and its value is only accessible within the module's scope.
+    .EXAMPLE
+        PS C:\> Get-ShlinkVists
+        
+        Returns the overall visit count for your Shlink server
+    .EXAMPLE
+        PS C:\> Get-ShlinkVisits -ShortCode "profile"
+        
+        Returns all visit data associated with the short code "profile"
+    .EXAMPLE
+        PS C:\> Get-ShlinkVisits -Tag "oldwebsite"
+
+        Returns all the visit data for all short codes asociated with the tag "oldwebsite"
+    .EXAMPLE
+        PS C:\> Get-ShlinkVisits -ShortCode "profile" -StartDate (Get-Date "2020-11-01") -EndDate (Get-Date "2020-11-30")
+
+        Returns all visit data associated with the short code "profile" for the whole of November 2020
+    .INPUTS
+        This function does not accept pipeline input.
+    .OUTPUTS
+        System.Management.Automation.PSObject
+    #>
     [CmdletBinding(DefaultParameterSetName="Server")]
     param (
         [Parameter(Mandatory, ParameterSetName="ShortCode")]
