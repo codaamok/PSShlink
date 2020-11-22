3035195e778aef9431165b901e0572127fb149ab
Adam Cook <me@cookadam.co.uk>
2020-11-22 11:27:19 +0000

    Added Shlink server version check to GetShlinkConnection

--- PSShlink/Private/GetShlinkConnection.ps1
+++ PSShlink/Private/GetShlinkConnection.ps1
@@ -9,6 +9,7 @@ function GetShlinkConnection {
         [Parameter()]
         [Switch]$ServerOnly
     )
+
     if (-not $Script:ShlinkServer) {
         if ([String]::IsNullOrWhitespace($Server)) {
             $Script:ShlinkServer = Read-Host -Prompt "Enter your Shlink server URL (e.g. https://example.com)"
@@ -25,4 +26,24 @@ function GetShlinkConnection {
             $Script:ShlinkApiKey = $ApiKey
         }
     }
+
+    # Query the Shlink server for version, only on the first run of GetShlinkConnection, otherwise it
+    # will enter an infinite loop of recursion and hit the PowerShell recursion limit. I want a user
+    # experience of being warned each time they use a function on an unsupported Shlink server version.
+    if (-not $Script:GetShlinkConnectionHasRun) {
+        $Script:GetShlinkConnectionHasRun = $true
+        $Script:ShlinkVersion = Get-ShlinkServer | Select-Object -ExpandProperty Version
+        $Script:MinSupportedShlinkVersion = [Version]"2.4.0"
+
+        if ([Version]$Script:ShlinkVersion -lt [Version]$Script:MinSupportedShlinkVersion) {
+            $Script:ShlinkVersionIsSupported = $false
+        }
+        else {
+            $Script:ShlinkVersionIsSupported = $true
+        }
+    }
+
+    if ($Script:ShlinkVersionIsSupported -eq $false) {
+        Write-Warning -Message ("PSShlink supports Shlink {0} or newer, your Shlink server is {1}. Some functions may not work as intended. Consider upgrading your Shlink instance." -f $Script:MinSupportedShlinkVersion, $Script:ShlinkVersion)
+    }    
 }
