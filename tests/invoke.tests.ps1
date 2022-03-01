e4f039a114feb1480d2986f85f9c13893a23c3c4
codaamok <me@cookadam.co.uk>
2022-03-01 22:24:15 +0000

    Updated .gitignore, pipeline.yml, and invoke.tests.ps1

--- tests/invoke.tests.ps1
+++ tests/invoke.tests.ps1
@@ -1,18 +1,22 @@
 $PesterConfig = New-PesterConfiguration -Hashtable @{
     Run = @{
         Path = @(
-            "{0}/Public/New-*.Acceptance.Tests.ps1" -f $PSScriptRoot
-            "{0}/Public/Get-*.Acceptance.Tests.ps1" -f $PSScriptRoot
-            "{0}/Public/Save-*.Acceptance.Tests.ps1" -f $PSScriptRoot
-            "{0}/Public/Set-ShlinkUrl.Acceptance.Tests.ps1" -f $PSScriptRoot
-            "{0}/Public/Set-ShlinkTag.Acceptance.Tests.ps1" -f $PSScriptRoot
-            "{0}/Public/Remove-*.Acceptance.Tests.ps1" -f $PSScriptRoot
+            '{0}/Public/New-*.Acceptance.Tests.ps1' -f $PSScriptRoot
+            '{0}/Public/Get-*.Acceptance.Tests.ps1' -f $PSScriptRoot
+            '{0}/Public/Save-*.Acceptance.Tests.ps1' -f $PSScriptRoot
+            '{0}/Public/Set-ShlinkUrl.Acceptance.Tests.ps1' -f $PSScriptRoot
+            '{0}/Public/Set-ShlinkTag.Acceptance.Tests.ps1' -f $PSScriptRoot
+            '{0}/Public/Remove-*.Acceptance.Tests.ps1' -f $PSScriptRoot
         )
         Throw = $true
         SkipRemainingOnFailure = 'Block'
     }
     Output = @{
         Verbosity = 'Detailed'
+    }
+    TestResult = @{
+        Enabled = $true
+        OutputFormat = 'NUnit2.5'
     }
 }
 
