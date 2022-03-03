deb7d8cc3a71915302cc6a48bd17be2cd1e164de
codaamok <me@cookadam.co.uk>
2022-03-03 20:29:20 +0000

    Updated Save-ShlinkUrlQrCode.Acceptance.Tests.ps1 +semver: skip

--- tests/Public/Save-ShlinkUrlQrCode.Acceptance.Tests.ps1
+++ tests/Public/Save-ShlinkUrlQrCode.Acceptance.Tests.ps1
@@ -4,21 +4,15 @@ BeforeAll {
     if (-not (Get-Module PSShlink)) {
         Import-Module $PSScriptRoot/../../src/PSShlink.psd1 -Force
     }
-
-    $TempFile = New-TemporaryFile -ErrorAction "Stop"
-}
-
-AfterAll {
-    Remove-Item $TempFile -ErrorAction 'Continue'
 }
 
 Describe "Save-ShlinkUrlQrCode" {
-    It "Create '<Format>' QR code image file of expected hash" -TestCases @(
+    It "Create '<Format>' QR code image file of expected SHA256 hash '<SHA256>'" -TestCases @(
         @{ Format = 'png'; SHA256 = 'A3D4EA74661878D8AB15AD864705AB43A095D5B60416F0B33F3E5212EEB527EF' }
         @{ Format = 'svg'; SHA256 = '380E532048FD830255CF8C34579D92813CB0B78F131D0E5790257A3155943391' }
     ) {
         $Params = @{
-            Path            = $TempFile.DirectoryName
+            Path            = $TestDrive
             ShortCode       = 'PSShlink-Test'
             Format          = $Format
             Size            = 100
@@ -33,7 +27,5 @@ Describe "Save-ShlinkUrlQrCode" {
         $File = Save-ShlinkUrlQrCode @Params
 
         (Get-FileHash $File -Algorithm SHA256).Hash | Should -Be $SHA256
-
-        Remove-Item $File -ErrorAction 'Continue'
     }
 }
